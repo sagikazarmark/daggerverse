7bee7015776014bc381a772b8e278ebc8ebab7b7
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-09-27 01:14:19 +0200

    add bats module
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ bats/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+)
+
+type Bats struct{}
+
+func (m *Bats) Bats(ctx context.Context, dir *Directory, args []string) (*Container, error) {
+	return run(dir, args), nil
+}
+
+func (dir *Directory) Bats(ctx context.Context, args []string) (string, error) {
+	return run(dir, args).Stdout(ctx)
+}
+
+func run(dir *Directory, args []string) *Container {
+	return image().
+		WithMountedDirectory("/src", dir).
+		WithWorkdir("/src").
+		WithExec(args)
+}
+
+func image() *Container {
+	return dag.
+		Container().
+		From("bats/bats:v1.10.0")
+}
