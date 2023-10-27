faf2d356d75ab4507084a3f5ac4ee831f13141ac
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-10-28 01:29:29 +0200

    update flake
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv";
+    dagger.url = "github:sagikazarmark/dagger-flake";
+    dagger.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: rec {
+        devenv.shells = {
+          default = {
+            languages = {
+              go.enable = true;
+            };
+
+            packages = with pkgs; [
+              golangci-lint
+            ] ++ [
+              inputs'.dagger.packages.dagger
+            ];
+
+            # https://github.com/cachix/devenv/issues/528#issuecomment-1556108767
+            containers = pkgs.lib.mkForce { };
+          };
+
+          ci = devenv.shells.default;
+        };
+      };
+    };
+}
