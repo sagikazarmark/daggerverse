85b7219631eed5343dcdbe98946327793e9348a7
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-10-28 01:31:25 +0200

    feat: rewrite bats
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- bats/main.go
+++ bats/main.go
@@ -1,32 +1,70 @@
 package main
 
 import (
-	"context"
+	"fmt"
 )
+
+// defaultImageRepository is used when no image is specified.
+const defaultImageRepository = "bats/bats"
 
 type Bats struct{}
 
-func (m *Bats) Bats(ctx context.Context, d *Directory, args []string) (*Container, error) {
-	return run(d, args), nil
+// Specify which version of Bats to use.
+func (m *Bats) WithVersion(version string) *BatsContainer {
+	return &BatsContainer{dag.Container().From(fmt.Sprintf("%s:%s", defaultImageRepository, version))}
 }
 
-func (m *Bats) Foo(ctx context.Context) (*Directory, error) {
-	return nil, nil
+// Specify a custom image reference in "repository:tag" format.
+func (m *Bats) WithImageRef(ref string) *BatsContainer {
+	return &BatsContainer{dag.Container().From(ref)}
 }
 
-func (d *Directory) Bats(ctx context.Context, args []string) (*Container, error) {
-	return run(d, args), nil
+// Specify a custom container.
+func (m *Bats) WithContainer(ctr *Container) *BatsContainer {
+	return &BatsContainer{ctr}
 }
 
-func run(dir *Directory, args []string) *Container {
-	return image().
-		WithMountedDirectory("/src", dir).
-		WithWorkdir("/src").
-		WithExec(args)
+// Mount a source directory.
+func (m *Bats) WithSource(src *Directory) *BatsContainerWithSource {
+	return defaultContainer().WithSource(src)
 }
 
-func image() *Container {
-	return dag.
-		Container().
-		From("bats/bats:v1.10.0")
+// Return the default container.
+func (m *Bats) Container() *Container {
+	return defaultContainer().Container()
+}
+
+func defaultContainer() *BatsContainer {
+	return &BatsContainer{dag.Container().From(defaultImageRepository)}
+}
+
+type BatsContainer struct {
+	Ctr *Container
+}
+
+// Return the underlying container.
+func (m *BatsContainer) Container() *Container {
+	return m.Ctr
+}
+
+// Mount a source directory.
+func (m *BatsContainer) WithSource(src *Directory) *BatsContainerWithSource {
+	const workdir = "/src"
+
+	return &BatsContainerWithSource{
+		&BatsContainer{
+			m.Ctr.
+				WithWorkdir(workdir).
+				WithMountedDirectory(workdir, src),
+		},
+	}
+}
+
+type BatsContainerWithSource struct {
+	*BatsContainer
+}
+
+// example usage: "dagger call with-source --src . lint --document openapi.yaml"
+func (m *BatsContainerWithSource) Run(args []string) *Container {
+	return m.Ctr.WithExec(args)
 }
--- ci/main.go
+++ ci/main.go
@@ -2,10 +2,10 @@ package main
 
 type Ci struct{}
 
-func (m *Ci) Spectral() *Container {
-	return dag.Spectral().
-		WithSource(dag.Host().Directory("./testdata/spectral")).
-		Lint("openapi.json")
+func (m *Ci) Bats() *Container {
+	return dag.Bats().
+		WithSource(dag.Host().Directory("./testdata/bats")).
+		Run([]string{"test.bats"})
 }
 
 func (m *Ci) Kafka() *Container {
@@ -14,4 +14,10 @@ func (m *Ci) Kafka() *Container {
 	return kafka.Container().
 		WithServiceBinding("kafka", kafka.Service()).
 		WithExec([]string{"kafka-topics.sh", "--list", "--bootstrap-server", "kafka:9092"})
+}
+
+func (m *Ci) Spectral() *Container {
+	return dag.Spectral().
+		WithSource(dag.Host().Directory("./testdata/spectral")).
+		Lint("openapi.json")
 }
