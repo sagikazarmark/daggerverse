d5da86877cae930fa6a6e2e6377cea565e5e0c62
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-11-06 14:57:37 -0600

    update bats
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- bats/README.md
+++ bats/README.md
@@ -12,6 +12,14 @@ dag.Bats().
     .Run([]string{"test.bats"})
 ```
 
+### Shell
+
+Run the following command to see the command line interface:
+
+```shell
+dagger call -m "github.com/sagikazarmark/daggerverse/spectral@main" --help
+```
+
 ## To Do
 
 - [ ] Custom container with additional tools installed
--- bats/main.go
+++ bats/main.go
@@ -9,24 +9,44 @@ const defaultImageRepository = "bats/bats"
 
 type Bats struct{}
 
-// Specify which version of Bats to use.
-func (m *Bats) WithVersion(version string) *BatsContainer {
-	return &BatsContainer{dag.Container().From(fmt.Sprintf("%s:%s", defaultImageRepository, version))}
+// Specify which version (image tag) of Bats to use from the official image repository on Docker Hub.
+func (m *Bats) FromVersion(version string) *Base {
+	return &Base{dag.Container().From(fmt.Sprintf("%s:%s", defaultImageRepository, version))}
 }
 
 // Specify a custom image reference in "repository:tag" format.
-func (m *Bats) WithImageRef(ref string) *BatsContainer {
-	return &BatsContainer{dag.Container().From(ref)}
+func (m *Bats) FromImage(image string) *Base {
+	return &Base{dag.Container().From(image)}
 }
 
 // Specify a custom container.
-func (m *Bats) WithContainer(ctr *Container) *BatsContainer {
-	return &BatsContainer{ctr}
+func (m *Bats) FromContainer(ctr *Container) *Base {
+	return &Base{ctr}
+}
+
+func defaultContainer() *Base {
+	return &Base{dag.Container().From(defaultImageRepository)}
 }
 
 // Mount a source directory.
-func (m *Bats) WithSource(src *Directory) *BatsContainerWithSource {
+func (m *Bats) WithSource(src *Directory) *BaseWithSource {
 	return defaultContainer().WithSource(src)
+}
+
+func (m *Bats) Run(args []string, version Optional[string], image Optional[string], container Optional[*Container], source Optional[*Directory]) *Container {
+	var base *Base
+
+	if v, ok := version.Get(); ok {
+		base = m.FromVersion(v)
+	} else if i, ok := image.Get(); ok {
+		base = m.FromImage(i)
+	} else if c, ok := container.Get(); ok {
+		base = m.FromContainer(c)
+	} else {
+		base = defaultContainer()
+	}
+
+	return base.Run(args, source)
 }
 
 // Return the default container.
@@ -34,25 +54,21 @@ func (m *Bats) Container() *Container {
 	return defaultContainer().Container()
 }
 
-func defaultContainer() *BatsContainer {
-	return &BatsContainer{dag.Container().From(defaultImageRepository)}
-}
-
-type BatsContainer struct {
+type Base struct {
 	Ctr *Container
 }
 
 // Return the underlying container.
-func (m *BatsContainer) Container() *Container {
+func (m *Base) Container() *Container {
 	return m.Ctr
 }
 
 // Mount a source directory.
-func (m *BatsContainer) WithSource(src *Directory) *BatsContainerWithSource {
+func (m *Base) WithSource(src *Directory) *BaseWithSource {
 	const workdir = "/src"
 
-	return &BatsContainerWithSource{
-		&BatsContainer{
+	return &BaseWithSource{
+		&Base{
 			m.Ctr.
 				WithWorkdir(workdir).
 				WithMountedDirectory(workdir, src),
@@ -60,11 +76,20 @@ func (m *BatsContainer) WithSource(src *Directory) *BatsContainerWithSource {
 	}
 }
 
-type BatsContainerWithSource struct {
-	*BatsContainer
+func (m *Base) Run(args []string, source Optional[*Directory]) *Container {
+	ctr := m.Ctr
+
+	if src, ok := source.Get(); ok {
+		ctr = m.WithSource(src).Ctr
+	}
+
+	return ctr.WithExec(args)
 }
 
-// example usage: "dagger call with-source --src . lint --document openapi.yaml"
-func (m *BatsContainerWithSource) Run(args []string) *Container {
+type BaseWithSource struct {
+	*Base
+}
+
+func (m *BaseWithSource) Run(args []string) *Container {
 	return m.Ctr.WithExec(args)
 }
