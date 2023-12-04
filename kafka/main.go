a7593d08502e8a1ed18cf538bfc5b28314fd0302
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-12-04 10:57:26 +0100

    refactor: kafka module to use the new from style functions
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- kafka/main.go
+++ kafka/main.go
@@ -7,23 +7,47 @@ const defaultImageRepository = "bitnami/kafka"
 
 type Kafka struct{}
 
-// Specify which version of Kafka to use.
-func (m *Kafka) WithVersion(version string) *KafkaContainer {
-	return &KafkaContainer{dag.Container().From(fmt.Sprintf("%s:%s", defaultImageRepository, version))}
+// Specify which version (image tag) of Kafka to use from the official image repository on Docker Hub.
+func (m *Kafka) FromVersion(version string) *Base {
+	return &Base{wrapContainer(dag.Container().From(fmt.Sprintf("%s:%s", defaultImageRepository, version)))}
 }
 
 // Specify a custom image reference in "repository:tag" format.
 //
 // Note: this module expects an image compatible with bitnami/kafka.
-func (m *Kafka) WithImageRef(ref string) *KafkaContainer {
-	return &KafkaContainer{dag.Container().From(ref)}
+func (m *Kafka) FromImage(ref string) *Base {
+	return &Base{wrapContainer(dag.Container().From(ref))}
 }
 
 // Specify a custom container.
 //
 // Note: this module expects an image compatible with bitnami/kafka.
-func (m *Kafka) WithContainer(ctr *Container) *KafkaContainer {
-	return &KafkaContainer{ctr}
+func (m *Kafka) FromContainer(ctr *Container) *Base {
+	return &Base{wrapContainer(ctr)}
+}
+
+func defaultContainer() *Base {
+	return &Base{wrapContainer(dag.Container().From(defaultImageRepository))}
+}
+
+func wrapContainer(c *Container) *Container {
+	return c.
+		// KRaft settings
+		WithEnvVariable("KAFKA_CFG_NODE_ID", "0").
+		WithEnvVariable("KAFKA_CFG_PROCESS_ROLES", "controller,broker").
+		WithEnvVariable("KAFKA_CFG_CONTROLLER_QUORUM_VOTERS", "0@127.0.0.1:9093").
+		// Listeners
+		WithEnvVariable("KAFKA_CFG_LISTENERS", "PLAINTEXT://:9092,CONTROLLER://:9093").
+		WithEnvVariable("KAFKA_CFG_ADVERTISED_LISTENERS", "PLAINTEXT://kafka:9092").
+		WithEnvVariable("KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP", "CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT").
+		WithEnvVariable("KAFKA_CFG_CONTROLLER_LISTENER_NAMES", "CONTROLLER").
+		WithEnvVariable("KAFKA_CFG_INTER_BROKER_LISTENER_NAME", "PLAINTEXT").
+		WithExposedPort(9092)
+}
+
+// Set an environment variable.
+func (m *Kafka) WithEnvVariable(name string, value string, expand Optional[bool]) *Base {
+	return defaultContainer().WithEnvVariable(name, value, expand)
 }
 
 // Return the default container.
@@ -36,32 +60,25 @@ func (m *Kafka) Service() *Service {
 	return defaultContainer().Service()
 }
 
-func defaultContainer() *KafkaContainer {
-	return &KafkaContainer{dag.Container().From(defaultImageRepository)}
-}
-
-type KafkaContainer struct {
+type Base struct {
 	Ctr *Container
 }
 
 // Return the underlying container.
-func (m *KafkaContainer) Container() *Container {
+func (m *Base) Container() *Container {
 	return m.Ctr
 }
 
+// Set an environment variable.
+func (m *Base) WithEnvVariable(name string, value string, expand Optional[bool]) *Base {
+	return &Base{
+		m.Ctr.WithEnvVariable(name, value, ContainerWithEnvVariableOpts{
+			Expand: expand.GetOr(false),
+		}),
+	}
+}
+
 // Launch a Kafka service container.
-func (m *KafkaContainer) Service() *Service {
-	return m.Ctr.
-		// KRaft settings
-		WithEnvVariable("KAFKA_CFG_NODE_ID", "0").
-		WithEnvVariable("KAFKA_CFG_PROCESS_ROLES", "controller,broker").
-		WithEnvVariable("KAFKA_CFG_CONTROLLER_QUORUM_VOTERS", "0@127.0.0.1:9093").
-		// Listeners
-		WithEnvVariable("KAFKA_CFG_LISTENERS", "PLAINTEXT://:9092,CONTROLLER://:9093").
-		WithEnvVariable("KAFKA_CFG_ADVERTISED_LISTENERS", "PLAINTEXT://kafka:9092").
-		WithEnvVariable("KAFKA_CFG_LISTENER_SECURITY_PROTOCOL_MAP", "CONTROLLER:PLAINTEXT,PLAINTEXT:PLAINTEXT").
-		WithEnvVariable("KAFKA_CFG_CONTROLLER_LISTENER_NAMES", "CONTROLLER").
-		WithEnvVariable("KAFKA_CFG_INTER_BROKER_LISTENER_NAME", "PLAINTEXT").
-		WithExposedPort(9092).
-		AsService()
+func (m *Base) Service() *Service {
+	return m.Ctr.AsService()
 }
