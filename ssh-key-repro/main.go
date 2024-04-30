34c3e978f32973d87c56c51442f90aa2ab193a12
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-04-30 12:43:47 +0200

    add repro code
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ ssh-key-repro/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "context"
+
+type SshKeyRepro struct{}
+
+func (m *SshKeyRepro) TestOk() *Container {
+	return dag.
+		Wolfi().
+		Container(WolfiContainerOpts{
+			Packages: []string{"git", "openssh"},
+		}).
+		WithMountedFile("/ssh-key", dag.CurrentModule().Source().File("id_ed25519")).
+		WithExec([]string{"ssh-keygen", "-y", "-f", "/ssh-key"})
+}
+
+func (m *SshKeyRepro) TestOkToo(ctx context.Context) *Container {
+	sshKeyContents, _ := dag.CurrentModule().Source().File("id_ed25519").Contents(ctx)
+
+	sshKey := dag.SetSecret("ssh-key", sshKeyContents)
+
+	return dag.
+		Wolfi().
+		Container(WolfiContainerOpts{
+			Packages: []string{"git", "openssh"},
+		}).
+		WithMountedSecret("/ssh-key", sshKey).
+		WithExec([]string{"ssh-keygen", "-y", "-f", "/ssh-key"})
+}
+
+func (m *SshKeyRepro) TestFail(ctx context.Context, sshKey *Secret) *Container {
+	// sshKeyContents, _ := dag.CurrentModule().Source().File("id_ed25519").Contents(ctx)
+	//
+	// sshKey := dag.SetSecret("ssh-key", sshKeyContents)
+
+	return dag.
+		Wolfi().
+		Container(WolfiContainerOpts{
+			Packages: []string{"git", "openssh"},
+		}).
+		WithMountedSecret("/ssh-key", sshKey).
+		WithExec([]string{"ssh-keygen", "-y", "-f", "/ssh-key"})
+}
