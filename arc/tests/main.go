9926aefee00a0f30af7e92402bf1a6885f473f76
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-05-15 13:05:15 +0200

    test(arc): improve tests

--- arc/tests/main.go
+++ arc/tests/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"slices"
 
 	"github.com/sourcegraph/conc/pool"
 )
@@ -12,21 +14,54 @@ type Tests struct{}
 func (m *Tests) All(ctx context.Context) error {
 	p := pool.New().WithErrors().WithContext(ctx)
 
+	p.Go(m.ArchiveFiles)
 	p.Go(m.ArchiveDirectory)
 
 	return p.Wait()
 }
 
-func (m *Tests) ArchiveDirectory(_ context.Context) error {
+func (m *Tests) ArchiveFiles(ctx context.Context) error {
+	dir := dag.CurrentModule().Source().Directory("./testdata")
+
+	archive := dag.Arc().
+		ArchiveFiles(
+			"test",
+			[]*File{
+				dir.File("hello"),
+				dir.File("foo/bar"),
+			},
+		).
+		TarGz()
+
+	unarchivedDir := dag.Arc().Unarchive(archive)
+
+	entries, err := unarchivedDir.Directory("test").Entries(ctx)
+	if err != nil {
+		return err
+	}
+
+	if !slices.Equal(entries, []string{"bar", "hello"}) {
+		return fmt.Errorf("unexpected entries: %v", entries)
+	}
+
+	return nil
+}
+
+func (m *Tests) ArchiveDirectory(ctx context.Context) error {
 	dir := dag.CurrentModule().Source().Directory("./testdata")
 
 	archive := dag.Arc().ArchiveDirectory("test", dir).TarGz()
 
 	unarchivedDir := dag.Arc().Unarchive(archive)
 
-	// TODO: improve test
+	entries, err := unarchivedDir.Directory("test").Entries(ctx)
+	if err != nil {
+		return err
+	}
 
-	_ = unarchivedDir
+	if !slices.Equal(entries, []string{"foo", "hello"}) {
+		return fmt.Errorf("unexpected entries: %v", entries)
+	}
 
 	return nil
 }
