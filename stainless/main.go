293c55a7b84f9a40ecf8f6bd76fe926ffa4a5e7a
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-05-16 09:54:56 +0200

    feat(stainless): create module

---
+++ stainless/main.go
@@ -0,0 +1,111 @@
+// Stainless is an API SDK generator tool.
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+)
+
+type Stainless struct {
+	// +private
+	Token *Secret
+}
+
+func New(token *Secret) *Stainless {
+	return &Stainless{
+		Token: token,
+	}
+}
+
+func (m *Stainless) UploadSpec(
+	ctx context.Context,
+
+	// Stainless project name.
+	projectName string,
+
+	// OpenAPI spec file.
+	openapi *File,
+
+	// Stainless config file.
+	//
+	// +optional
+	config *File,
+
+	// Commit message (following conventional commit format).
+	//
+	// +optional
+	commitMessage string,
+) (*File, error) {
+	var buf bytes.Buffer
+
+	body := multipart.NewWriter(&buf)
+
+	err := body.WriteField("projectName", projectName)
+	if err != nil {
+		return nil, err
+	}
+
+	if commitMessage != "" {
+		err := body.WriteField("commitMesssage", commitMessage)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = writeFormFile(ctx, body, "oasSpec", openapi, "openapi.json")
+	if err != nil {
+		return nil, err
+	}
+
+	if config != nil {
+		err = writeFormFile(ctx, body, "stainlessConfig", config, "stainless.yaml")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://api.stainlessapi.com/api/spec", &buf)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := m.Token.Plaintext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	request.Header.Set("Authorization", "Bearer "+token)
+	request.Header.Set("Content-Type", body.FormDataContentType())
+
+	// TODO: customize HTTP client (timeout, etc)
+	client := http.DefaultClient
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		respBody, _ := io.ReadAll(response.Body)
+
+		return nil, fmt.Errorf("failed to upload files: %s %s", response.Status, respBody)
+	}
+
+	decoratedSpec, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return dag.Directory().WithNewFile("openapi.json", string(decoratedSpec)).File("openapi.json"), nil
+}
