9e4e0222e0cbf4f07ee377c1723889dbb4173b5e
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-06-12 17:57:03 +0200

    feat(xcaddy): polish API, add new features
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- xcaddy/tests/main.go
+++ xcaddy/tests/main.go
@@ -16,18 +16,14 @@ func (m *Tests) All(ctx context.Context) error {
 
 	p.Go(m.Test)
 	p.Go(m.Version)
-	p.Go(m.WithVersion)
-	p.Go(m.WithModule().All)
+	p.Go(m.Plugin().All)
 
 	return p.Wait()
 }
 
 func (m *Tests) Test(ctx context.Context) error {
-	binary := dag.Xcaddy().Build().Binary()
-
-	caddyService := dag.Container().
+	caddyService := dag.Xcaddy().Build().Container().
 		From("caddy").
-		WithFile("/usr/bin/caddy", binary).
 		WithFile("/etc/caddy/Caddyfile", dag.CurrentModule().Source().File("Caddyfile")).
 		WithExposedPort(80).
 		AsService()
@@ -50,7 +46,7 @@ func (m *Tests) Test(ctx context.Context) error {
 }
 
 func (m *Tests) Version(ctx context.Context) error {
-	binary := dag.Xcaddy().Build(XcaddyBuildOpts{Version: "latest"}).Binary()
+	binary := dag.Xcaddy().Build(XcaddyBuildOpts{Version: "v2.8.4"}).Binary()
 
 	_, err := dag.Container().
 		From("alpine").
@@ -61,27 +57,14 @@ func (m *Tests) Version(ctx context.Context) error {
 	return err
 }
 
-func (m *Tests) WithVersion(ctx context.Context) error {
-	binary := dag.Xcaddy().Build(XcaddyBuildOpts{Version: "latest"}).WithVersion("v2.8.4").Binary()
-
-	_, err := dag.Container().
-		From("alpine").
-		WithFile("/usr/local/bin/caddy", binary).
-		// TODO: make sure version is correct
-		WithExec([]string{"caddy", "version"}).
-		Sync(ctx)
-
-	return err
+func (m *Tests) Plugin() *Plugin {
+	return &Plugin{}
 }
 
-func (m *Tests) WithModule() *WithModule {
-	return &WithModule{}
-}
-
-type WithModule struct{}
+type Plugin struct{}
 
 // All executes all tests.
-func (m *WithModule) All(ctx context.Context) error {
+func (m *Plugin) All(ctx context.Context) error {
 	p := pool.New().WithErrors().WithContext(ctx)
 
 	p.Go(m.Module)
@@ -91,10 +74,10 @@ func (m *WithModule) All(ctx context.Context) error {
 	return p.Wait()
 }
 
-func (m *WithModule) Module(ctx context.Context) error {
+func (m *Plugin) Module(ctx context.Context) error {
 	binary := dag.Xcaddy().
 		Build().
-		WithModule("github.com/sagikazarmark/caddy-fs-s3").
+		Plugin("github.com/sagikazarmark/caddy-fs-s3").
 		Binary()
 
 	_, err := dag.Container().
@@ -106,10 +89,10 @@ func (m *WithModule) Module(ctx context.Context) error {
 	return err
 }
 
-func (m *WithModule) Version(ctx context.Context) error {
+func (m *Plugin) Version(ctx context.Context) error {
 	binary := dag.Xcaddy().
 		Build().
-		WithModule("github.com/sagikazarmark/caddy-fs-s3", dagger.XcaddyBuildWithModuleOpts{
+		Plugin("github.com/sagikazarmark/caddy-fs-s3", dagger.XcaddyBuildPluginOpts{
 			Version: "v0.3.1",
 		}).
 		Binary()
@@ -123,12 +106,12 @@ func (m *WithModule) Version(ctx context.Context) error {
 	return err
 }
 
-func (m *WithModule) Replacement(ctx context.Context) error {
+func (m *Plugin) Replacement(ctx context.Context) error {
 	binary := dag.Xcaddy().
 		Build().
-		WithModule("github.com/sagikazarmark/caddy-fs-s3", dagger.XcaddyBuildWithModuleOpts{
+		Plugin("github.com/sagikazarmark/caddy-fs-s3", dagger.XcaddyBuildPluginOpts{
 			// TODO: lock to specific version?
-			Replacement: dag.Git("https://github.com/sagikazarmark/caddy-fs-s3.git").Branch("main").Tree(),
+			Replacement: dag.Git("https://github.com/sagikazarmark/caddy-fs-s3.git").Tag("v0.4.0").Tree(),
 		}).
 		Binary()
 
