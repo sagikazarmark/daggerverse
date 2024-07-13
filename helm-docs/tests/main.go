cdcfcde5b9017acf656125505f911ff376d4aecf
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-07-13 12:19:06 +0200

    feat(helm-docs): upgrade dagger
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- helm-docs/tests/main.go
+++ helm-docs/tests/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"dagger/helm-docs/tests/internal/dagger"
 	"fmt"
 
 	"github.com/sourcegraph/conc/pool"
@@ -23,32 +24,32 @@ func (m *Tests) All(ctx context.Context) error {
 }
 
 func (m *Tests) Default(ctx context.Context) error {
-	return m.runTest(ctx, "default", HelmDocsGenerateOpts{})
+	return m.runTest(ctx, "default", dagger.HelmDocsGenerateOpts{})
 }
 
 func (m *Tests) SortValues(ctx context.Context) error {
-	return m.runTest(ctx, "sort-values", HelmDocsGenerateOpts{
+	return m.runTest(ctx, "sort-values", dagger.HelmDocsGenerateOpts{
 		SortValuesOrder: "file",
 	})
 }
 
 func (m *Tests) Template(ctx context.Context) error {
-	return m.runTest(ctx, "template", HelmDocsGenerateOpts{
-		Templates: []*File{
+	return m.runTest(ctx, "template", dagger.HelmDocsGenerateOpts{
+		Templates: []*dagger.File{
 			dag.CurrentModule().Source().File("./testdata/charts/template/template.md"),
 		},
 	})
 }
 
-func (m *Tests) runTest(ctx context.Context, chartName string, opts HelmDocsGenerateOpts) error {
-	chartDir := func(chartName string) *Directory {
+func (m *Tests) runTest(ctx context.Context, chartName string, opts dagger.HelmDocsGenerateOpts) error {
+	chartDir := func(chartName string) *dagger.Directory {
 		return dag.CurrentModule().Source().Directory(fmt.Sprintf("./testdata/charts/%s", chartName))
 	}
 
-	expected := func(chartName string) *File {
+	expected := func(chartName string) *dagger.File {
 		return dag.CurrentModule().Source().File(fmt.Sprintf("./testdata/charts/%s/expected.md", chartName))
 	}
-	actual := dag.HelmDocs(HelmDocsOpts{
+	actual := dag.HelmDocs(dagger.HelmDocsOpts{
 		Version: version,
 	}).
 		Generate(chartDir(chartName), opts)
