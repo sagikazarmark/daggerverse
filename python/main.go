163f0cbb71a16c9cccc6bdf9c30e1587c9f1ead8
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-07-13 12:44:01 +0200

    feat(python): upgrade dagger
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- python/main.go
+++ python/main.go
@@ -1,23 +1,28 @@
 // Python programming language module.
 package main
 
-import "fmt"
+import (
+	"dagger/python/internal/dagger"
+	"fmt"
+)
 
 // defaultImageRepository is used when no image is specified.
 const defaultImageRepository = "python"
 
 type Python struct {
-	Container *Container
+	Container *dagger.Container
 }
 
 func New(
 	// Version (image tag) to use from the official image repository as a base container.
+	//
 	// +optional
 	version string,
 
 	// Custom container to use as a base container.
+	//
 	// +optional
-	container *Container,
+	container *dagger.Container,
 ) *Python {
 	if container == nil {
 		if version == "" {
@@ -32,17 +37,19 @@ func New(
 
 // Mount a cache volume for Pip cache.
 func (m *Python) WithPipCache(
-	cache *CacheVolume,
+	cache *dagger.CacheVolume,
 
 	// Identifier of the directory to use as the cache volume's root.
+	//
 	// +optional
-	source *Directory,
+	source *dagger.Directory,
 
 	// Sharing mode of the cache volume.
+	//
 	// +optional
-	sharing CacheSharingMode,
+	sharing dagger.CacheSharingMode,
 ) *Python {
-	return &Python{m.Container.WithMountedCache("/root/.cache/pip", cache, ContainerWithMountedCacheOpts{
+	return &Python{m.Container.WithMountedCache("/root/.cache/pip", cache, dagger.ContainerWithMountedCacheOpts{
 		Source:  source,
 		Sharing: sharing,
 	})}
@@ -51,7 +58,7 @@ func (m *Python) WithPipCache(
 // Mount a source directory.
 func (m *Python) WithSource(
 	// Source directory to mount.
-	source *Directory,
+	source *dagger.Directory,
 ) *Python {
 	const workdir = "/work"
 
