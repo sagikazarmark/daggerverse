5ebe86bd0ef33572e195768dc7c56386ebb17472
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-08-07 00:23:55 +0200

    feat: add postgres module
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ postgres/main.go
@@ -0,0 +1,51 @@
+// A generated module for Postgres functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"dagger/postgres/internal/dagger"
+	"fmt"
+)
+
+// defaultImageRepository is used when no image is specified.
+const defaultImageRepository = "postgres"
+
+type Postgres struct {
+	Container *dagger.Container
+}
+
+func New(
+	// Version (image tag) to use from the official image repository as a base container.
+	//
+	// +optional
+	version string,
+
+	// Custom container to use as a base container. Takes precedence over version.
+	//
+	// +optional
+	container *dagger.Container,
+) (*Postgres, error) {
+	if container == nil {
+		if version == "" {
+			version = "latest"
+		}
+
+		container = dag.Container().From(fmt.Sprintf("%s:%s", defaultImageRepository, version))
+	}
+
+	return &Postgres{
+		Container: container,
+	}, nil
+}
