e846bc48e5e08b4bd63a2b4aba38c3b143455930
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2025-09-22 20:34:28 +0200

    build: replace flake with devenv
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ devenv.nix
@@ -0,0 +1,32 @@
+{ pkgs, inputs, ... }:
+
+{
+  cachix.pull = [ "sagikazarmark-dev" ];
+
+  overlays = [
+    (final: prev: {
+      dagger = inputs.dagger.packages.${final.system}.dagger;
+    })
+  ];
+
+  languages = {
+    go = {
+      enable = true;
+      package = pkgs.go_1_25;
+    };
+  };
+
+  packages = with pkgs; [
+    dagger
+    golangci-lint
+    just
+    git
+    semver-tool
+    jq
+    moreutils
+    fd
+
+    # is this still necessary?
+    yq-go
+  ];
+}
